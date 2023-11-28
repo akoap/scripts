c135b7d88c87805a91bfe286d88873eb2d797873
Akshay Patil <avpat02@gmail.com>
2023-11-27 21:04:09 -0500

    Add .cpp files to cpplint script

--- cpplint.sh
+++ cpplint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cpplint --linelength=120 --counting=detailed $( find . -name \*.h -or -name \*.cc | grep -vE "^\.\/build\/" )
+cpplint --linelength=120 --counting=detailed $( find . -name \*.h -or -name \*.cc -or -name \*.cpp | grep -vE "^\.\/build\/" )
 
